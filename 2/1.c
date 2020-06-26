2388f1da9b81658abfcd06217ebaf47c43b4a2f8
Doctorade <1300477780@qq.com>
2020-06-26 22:21:35 +0800

    2-debug

--- 2/1.c
+++ 2/1.c
@@ -1,43 +1,51 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    int matrix_a[10][10];
-    int matrix_b[10][10];
-    int m;
-    int n;
-	scanf("%d%d", &m, &n);
-    int i;
-    int j;
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
+int main()
+{
+    int matrix_a[10][10], matrix_b[10][10];
+    int m, n;
+	scanf("%d %d", &m, &n);
+	
+    int i, j;
+    for(i = 0; i < m; i++) 
+	{
+        for(j = 0; j < n; j++)
+		{
             scanf("%d", &matrix_a[i][j]);
         }
     }
-	for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
+	for(i = 0; i < n; i++) {
+        for(j = 0; j < m; j++)
+		{
             scanf("%d", &matrix_b[i][j]);
         }
     }
+    
     int sum[m][m];
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < m; j++) {
-            sum[m][m] = 0;
+	memset(sum, 0, sizeof(int)*m*m);
+    int digit;
+    for(i = 0; i < m; i++)
+	{
+        for(j = 0; j < m; j++)
+		{
+            for(digit = 0; digit < n; digit++)
+            {
+            	sum[i][j] = sum[i][j] + matrix_a[i][digit] * matrix_b[digit][j];
+			}
         }
     }
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < m; j++) {
-            for (int digit = 0; digit < n; digit++)
-            sum[i][j] = sum[i][j] + matrix_a[i][digit] * matrix_b[digit][j];
-        }
-    }
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < m; j++) {
+    for (i = 0; i < m; i++)
+	{
+        for (j = 0; j < m; j++){
             printf("%d", sum[i][j]);
-            if (j != m - 1) {
+            if(j != m - 1)
+			{
                 printf(" ");
             }
         }
         printf("\n");
     }
+    
     return 0;
 }
--- 2/4.c
+++ 2/4.c
@@ -1,61 +1,51 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
-    int matrix_a[10][10];
-    int matrix_b[10][10];
-    int m;
-    int n;
-    scanf("%d %d",&m,&n);
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            scanf("%d",&matrix_a[i][j]);
+
+int main()
+{
+    int matrix_a[10][10], matrix_b[10][10];
+    int m, n;
+    scanf("%d %d", &m, &n);
+    
+    int i, j, k;
+    for(i=0; i<m; i++)
+	{
+        for(j=0; j<n; j++)
+		{
+            scanf("%d", &matrix_a[i][j]);
         }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            scanf("%d",&matrix_b[i][j]);
+    for(i=0; i<n; i++)
+	{
+        for(j=0; j<m; j++)
+		{
+            scanf("%d", &matrix_b[i][j]);
         }
     }
-    /*
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            printf("%d ",matrix_a[i][j]);
-        }
-        printf("\n");
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            printf("%d ",matrix_b[i][j]);
-        }
-        printf("\n");
-    }
-    */
+    
     int m3[m][m];
-    memset(m3,0,sizeof(int)*m*m);
-    for(int i=0;i<m;i++){
-        for(int j=0;j<m;j++){
-            printf("%d ",m3[i][j]);
-        }
-        printf("\n");
-    }
-    //¾ØÕóÏà³Ë ·ÅÈëm3
-    for(int i=0;i<m;i++){
-        for(int j=0;j<m;j++){
-            for(int k=0;k<n;k++){
-                m3[i][j]+=matrix_a[i][k]*matrix_b[k][j];
+    memset(m3, 0, sizeof(int)*m*m);
+    for(i=0; i<m; i++)
+	{
+        for(j=0; j<m; j++)
+		{
+            for(k=0; k<n; k++)
+			{
+                m3[i][j] += matrix_a[i][k] * matrix_b[k][j];
             }
         }
     }
-    for(int i=0;i<m;i++){
-        for(int j=0;j<m;j++){
-            printf("%d",m3[i][j]);
-            if(j!=m-1){
+    for(i=0; i<m; i++)
+	{
+        for(j=0; j<m; j++)
+		{
+            printf("%d", m3[i][j]);
+            if(j != m-1)
+			{
                 printf(" ");
             }
         }
-        
         printf("\n");
-        
     }
 
     return 0;
