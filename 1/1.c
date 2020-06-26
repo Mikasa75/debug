6f4ad7a99cfae9ca92d8cdffef0bf9195aea778c
Doctorade <1300477780@qq.com>
2020-06-26 20:22:20 +0800

    1-debug

--- 1/1.c
+++ 1/1.c
@@ -42,4 +42,42 @@ double bisection(int p, int q, double (*func)(int, int, double)) {
 
 double f(int p, int q, double x) {
     return p * x + q;
+#include <stdio.h>
+#include <math.h>
+#define EPSILON 1e-7
+
+double bisection(int p, int q, double (*func)(int, int, double));
+double f(int p, int q, double x);
+
+int main()
+{
+    int p, q;
+    scanf("%d %d", &p, &q);
+    printf("%.4f\n", bisection(p, q, f));
+    return 0;
+}
+
+double bisection(int p, int q, double (*func)(int, int, double))
+{
+    double a = -20, b = 20, xp;
+    xp = (a + b) / 2;
+    while(fabs(func(p, q , xp)) >= EPSILON)
+    {
+        if((func(p, q , xp) * func(p, q , a)) > 0)
+        {
+    		a = xp;
+        	xp = (a + b) / 2;
+        }
+    	else
+        {
+            b = xp;
+            xp = (a + b) / 2;
+        }
+    }
+    return xp;
+}
+
+double f(int p, int q, double x)
+{
+    return p * x + q;
 }
