ef862930ca91803c669228d3635a3d6df74e65ec
Doctorade <1300477780@qq.com>
2020-06-29 22:54:38 +0800

    8-debug

--- 8/3.c
+++ 8/3.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     int n;
     scanf("%d\n", &n);
+    
     size_t max = 0;
     size_t current;
     char maxName[200];
     char name[200];
-    for (int i = 0; i < n; ++i) {
+    
+    int i;
+    for (i=0; i<n; ++i) {
         gets(name);
         current = strlen(name);
         if (current > max) {
@@ -16,6 +20,8 @@ int main() {
            strcpy(maxName, name);
         }
     }
+
     printf(maxName);
+    
     return 0;
 }
--- 8/4.c
+++ 8/4.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
+
+int main()
+{
     int n;
     scanf("%d\n", &n);
-    char name[n][100];
-    char temp[100];
-    
 
-    for(int i = 0; i < n; i++){
+    int i, j;
+    char name[n][101];
+    char temp[100];
+    for (i=0; i<n; i++) {
         gets(name[i]);        
     }
- 
-    for (int j = 0; j < n; j++){
-        for (int k = 0; k <= n - j; k++){
+    
+    for (j=0; j<n; j++) {
+        for (i=0; i<=n-j; i++) {
             char temp[20];
-            if((strlen(name[k]) < strlen(name[k + 1]))){
-                strcpy(temp, name[k]);
-                strcpy(name[k], name[k + 1]);
-                strcpy(name[k + 1], temp);
+            if((strlen(name[i]) < strlen(name[i + 1]))) {
+                strcpy(temp, name[i]);
+                strcpy(name[i], name[i + 1]);
+                strcpy(name[i + 1], temp);
             }
         }
     }
-    printf("%s\n", name[0]);
+    
+    printf("%s", name[0]);
+    
     return 0;
 }
