4eb8775996afeed5eca8f4427303c24ed3812c33
Doctorade <1300477780@qq.com>
2020-06-29 18:14:06 +0800

    9-debug

--- 9/3.c
+++ 9/3.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
-    long long int fun(int n)
-    {if(n==2)
-     return 1;
-    if(n==3)
-     return 1;
-    else
-     return fun(n-2)+fun(n-3);
-    }
-int main() {
-long long int n;
-    //int sum = 0;
+
+long long int fun(int n);
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
     
-    while(scanf("%d",&n)!=EOF){
-        
-    printf("%lld\n",fun(n));
-    }
+    printf("%lld", fun(n));
+    
     return 0;
+}
+
+long long int fun(int n)
+{
+    if (n == 0) { 
+    	return 0;
+    } else if (n == 1) {
+    	return 0;
+    } else if (n == 2) { 
+    	return 1;
+    } else if (n == 3) {
+    	return 1;
+    } else {
+        return fun(n-2) + fun(n-3);
+    }
 }
