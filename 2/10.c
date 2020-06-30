579dc37cd0882478da0b31bf3d42ba53bba305c3
mayifan <2447282215@qq.com>
2020-06-30 22:52:54 +0800

    rename .txt to .c and fix som bugs

---
+++ 2/10.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int matrix_a[10][10];
+    int matrix_b[10][10];
+    int result[10][10];
+    int m;
+    int n;
+    scanf("%d %d", &m, &n);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix_a[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &matrix_b[i][j]);
+        }
+    }
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < m; j++)
+        {
+            result[i][j] = 0;
+        }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < n; k++) {
+                result[i][j] += matrix_a[i][k] * matrix_b[k][j];
+            }
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d", result[i][j]);
+            if (j != m - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
---
+++ 2/9.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int matrix_a[10][10];
+    int matrix_b[10][10];
+    int matrix_[10][10];
+    int m;
+    int n;
+    scanf("%d %d", &m, &n);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix_a[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &matrix_b[i][j]);
+        }
+    }
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < m; j++)
+        {
+            matrix_[i][j] = 0;
+        }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < n; k++) {
+                matrix_[i][j] += matrix_a[i][k] * matrix_b[k][j];
+            }
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d", matrix_[i][j]);
+            if (j != m - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
