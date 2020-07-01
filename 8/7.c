9a7be225c7768b4a14ec6ddce7ccecff5ee8f1ad
mayifan <2447282215@qq.com>
2020-07-01 22:48:09 +0800

    fix bug

--- 8/7.c
+++ 8/7.c
@@ -4,6 +4,20 @@ int main() {
     int n;
     int i, j;
     scanf("%d", &n);
+      size_t max = 0;
+    size_t current;
+    char maxName[200];
+    char name[200];
+    for (int i = 0; i < n; ++i) {
+        gets(name);
+        current = strlen(name);
+        if (current > max) {
+           max = current;
+           strcpy(maxName, name);
+        }
+    }
+    printf(maxName);
+    return 0;
     char names[n][101];
     char temp[101];
     
