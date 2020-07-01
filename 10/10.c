b371d075130389ad350a76f4958a0093ec562aae
mayifan <2447282215@qq.com>
2020-07-01 22:56:35 +0800

    fix bugs

--- 10/10.c
+++ 10/10.c
@@ -2,7 +2,22 @@
 int main()
 {
     int N;
-    int i,j;
+  
+    int n, i, j;
+	
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		for (j = 2; j <= i; j++) {
+			if (i % j == 0) {
+				break;
+			}
+		}
+		if (j == i) {
+			printf("%d\n", i);
+		}
+	}
+
+	return 0;
     scanf("%d",&N);
     printf("2\n");
     for(i=3;i<N;i++)
--- 10/7.c
+++ 10/7.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 int main() {
-    int a;
+    
     int i;
     int j;
+    int a,b,c;
+    scanf("%d",&c);
+        
+     for(b=2;b<=c;b++) { 
+     for(a=2;a<b;a++){
+     if(b%a==0){
+         printf("");
+         break;
+     }
+     }
+        if (a>=b){
+            printf("%d\n",b);
+        }
+     }
+    return 0;
     scanf("%d",&a);
     if(a==2)
     {
