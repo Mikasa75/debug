0aea8ec4db4bd1378f1141aaf17409e767ceeb72
mayifan <2447282215@qq.com>
2020-07-01 23:15:14 +0800

    fix bugs

--- 12/9.c
+++ 12/9.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+9   #include <stdio.h>
+#include <math.h>
 #define PI 3.14159
 int main() {
-	float r;
-    float c, s;
-	scanf("%g", &r);
-    c = 2 * PI * r;
-    s = PI * r *r;
-    printf("%.3f\n",c);
-    printf("%.3f\n",s);
+    double r;
+    double c, s;
+    scanf("%lf", &r);
+    c = 2 * PI * r * 1000;
+    s = PI * r * r * 1000000;
+    printf("%.3f\n", (round(c) / 1000));
+    printf("%.3f\n", (round(s) / 1000000));
     return 0;
 }
