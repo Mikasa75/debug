836a8fa544c7cc50b48c753612fa48e769427445
mayifan <2447282215@qq.com>
2020-07-01 09:46:27 +0800

    .txt->.c and fix some bugs

---
+++ 6/7.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+void add(char);
+void delete(char);
+int rwx = 0;
+int main() {
+   char string[4];
+    scanf("%s",string);
+    int current = 0;
+    for (int i = 0; i < 3; i++) {
+        if (string[i] == 'r') {
+            current += 2 * 2;
+        }
+        else if (string[i] == 'w') {
+            current += 2;
+        }
+        else if (string[i] == 'x') {
+            current += 1;
+        }
+    }
+   
+    char reqrmt[3];
+    int req = 0;
+    while (scanf("%s", reqrmt) != EOF) {
+        if (reqrmt[1] == 'r') {
+            req = 2 * 2;
+        }
+        else if (reqrmt[1] == 'w') {
+            req = 2;
+        }
+        else {
+            req = 1;
+        }
+        if (reqrmt[0] == '+') {
+            current = current | req;
+        }
+        else {
+            current = current & (~req);
+        }
+      
+    }
+
+    printf("%d\n", current);
+    return 0;
+}
+void add(char input) {
+    if (input == 'r' && rwx / 4 != 1) rwx ^= (1 << 2);
+    else if (input == 'w' && (rwx / 2 % 2 != 1)) rwx ^= (1 << 1);
+    else if (rwx % 4 != 1) rwx ^= (1 << 0);
+}
+void delete(char input) {
+    if (input == 'r' && rwx / 4 == 1) rwx ^= (1 << 2);
+    else if (input == 'w' && (rwx / 2 % 2 == 1)) rwx ^= (1 << 1);
+    else if (rwx % 4 == 1) rwx ^= (1 << 0);
+}
--- 6/8.c
+++ 6/8.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 int main() {
     char n;
+    char string[4];
+    scanf("%s",string);
+    int current = 0;
+    for (int i = 0; i < 3; i++) {
+        if (string[i] == 'r') {
+            current += 2 * 2;
+        }
+        else if (string[i] == 'w') {
+            current += 2;
+        }
+        else if (string[i] == 'x') {
+            current += 1;
+        }
+    }
+   
+    char reqrmt[3];
+    int req = 0;
+    while (scanf("%s", reqrmt) != EOF) {
+        if (reqrmt[1] == 'r') {
+            req = 2 * 2;
+        }
+        else if (reqrmt[1] == 'w') {
+            req = 2;
+        }
+        else {
+            req = 1;
+        }
+        if (reqrmt[0] == '+') {
+            current = current | req;
+        }
+        else {
+            current = current & (~req);
+        }
+      
+    }
+
+    printf("%d\n", current);
+    return 0;
     int a[3]={0,0,0};
     for(int i=0;i<3;i++){
        scanf("%c",&n);
@@ -15,6 +53,8 @@ int main() {
         }
         
     }
+
+   
     while (scanf("%c" , &n) !=EOF){
        if(n == '\n'){
           continue;
