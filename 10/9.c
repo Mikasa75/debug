29b3b3dba09d52a1dc892c9c144eb24f23cfafb2
mayifan <2447282215@qq.com>
2020-07-01 10:18:47 +0800

    .txt->.c and fix some bugs

--- 10/9.c
+++ 10/9.c
@@ -3,6 +3,21 @@ int main() {
     int a;
     int b;
     int c;
+    int n, i, j;
+	
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		for (j = 2; j <= i; j++) {
+			if (i % j == 0) {
+				break;
+			}
+		}
+		if (j == i) {
+			printf("%d\n", i);
+		}
+	}
+
+	return 0;
     scanf("%d",&a);
     for(b == 2 ; b <= a ; b++){
         for(c == 2 ; c <= b ; c++){
